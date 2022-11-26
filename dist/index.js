1911c71d47623004f1416dcaa63c4a4508ba4d89
adamzolyak <adam.zolyak@gmail.com>
2022-11-26 15:41:50 -0700

    wip

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,6 @@
 "use strict";
-let sales = 123456789;
-let course = 'TypeScript';
-let is_published = true;
-let level;
-function render(document) {
-    console.log(document);
-}
-let numbers = [1, 2, 3];
-let user = [1, 'Adam'];
-var Size1;
-(function (Size1) {
-    Size1[Size1["Small"] = 0] = "Small";
-    Size1[Size1["Medium"] = 1] = "Medium";
-    Size1[Size1["Large"] = 2] = "Large";
-})(Size1 || (Size1 = {}));
-var Size2;
-(function (Size2) {
-    Size2[Size2["Small"] = 1] = "Small";
-    Size2[Size2["Medium"] = 2] = "Medium";
-    Size2[Size2["Large"] = 3] = "Large";
-})(Size2 || (Size2 = {}));
-var Size3;
-(function (Size3) {
-    Size3["Small"] = "s";
-    Size3["Medium"] = "m";
-    Size3["Large"] = "l";
-})(Size3 || (Size3 = {}));
-console.log(Size1, Size2, Size3, 'something white...');
+Object.defineProperty(exports, "__esModule", { value: true });
+const tax_1 = require("./tax");
+let tax = console.log((0, tax_1.calculateTax)(1000));
+console.log(tax);
 //# sourceMappingURL=index.js.map
---
+++ dist/shapes.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Square = exports.Circle = void 0;
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+}
+exports.Circle = Circle;
+class Square {
+    constructor(radius) {
+        this.radius = radius;
+    }
+}
+exports.Square = Square;
+//# sourceMappingURL=shapes.js.map
